7d81548f8f5366cf75317349cdd583a296f8cc31
mihai_coanda <coanda.mihai@gmail.com>
2017-09-27 16:27:10 +0300

    Removed console.log.

--- web/plugin.js
+++ web/plugin.js
@@ -1,5 +1,5 @@
 (function () {
   goog.events.listenOnce(workspace, sync.api.Workspace.EventType.BEFORE_EDITOR_LOADED, function(e) {
-    console.log('Monitoring plugin loaded.');
+    
   });
 })();
